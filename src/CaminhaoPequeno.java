c6996362d2d0f7197ec1706631e8335fba4c1f8c
MateusFariasS <mateus.fsantos08@gmail.com>
2025-03-30 15:00:52 -0300

    testes

--- src/CaminhaoPequeno.java
+++ src/CaminhaoPequeno.java
@@ -1,4 +1,56 @@
-package PACKAGE_NAME;
-
 public class CaminhaoPequeno {
+    private String id ;
+    private int capacidade;
+    private double cargaAtual;
+    private int viagensRealizadas;
+    private String status;
+
+    public CaminhaoPequeno(int capacidade){
+        this.id = id;
+        this.capacidade = capacidade;
+        this.cargaAtual = 0;
+        this.viagensRealizadas = 0;
+        this.status = "disponível";
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public double getCargaAtual() {
+        return cargaAtual;
+    }
+
+    public void setCargaAtual(double cargaAtual) {
+        this.cargaAtual = cargaAtual;
+    }
+
+    public int getViagensRealizadas() {
+        return viagensRealizadas;
+    }
+
+    public void setViagensRealizadas(int viagensRealizadas) {
+        this.viagensRealizadas = viagensRealizadas;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
